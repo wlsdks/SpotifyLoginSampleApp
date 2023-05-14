3dd174d70646c9e849942be96bf25cb57a0ae018
jinan <ja.choi@sweetk.co.kr>
2023-05-14 16:38:39 +0900

    1. outlet변수 및 action연결

--- SpotifyLoginSampleApp/LoginViewController.swift
+++ SpotifyLoginSampleApp/LoginViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 class LoginViewController: UIViewController {
     
     
+    @IBOutlet weak var emailLoginButton: UIButton!
+    @IBOutlet weak var googleLoginButton: UIButton!
+    @IBOutlet weak var appleLoginButton: UIButton!
     
     
     
+    // MARK: - 맨 처음 실행
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        [emailLoginButton, googleLoginButton, appleLoginButton].forEach {
+            $0?.layer.borderWidth = 1
+            $0?.layer.borderColor = UIColor.white.cgColor
+            $0?.layer.cornerRadius = 30
+        }
+    }
+    
+    // MARK: - view가 보일때마다 동작
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -22,7 +37,14 @@ class LoginViewController: UIViewController {
     }
     
     
+    // MARK: - action함수 추가
+    @IBAction func googleLoginButtonTapped(_ sender: UIButton) {
+        
+    }
     
+    @IBAction func appleLoginButtonTapped(_ sender: UIButton) {
+        
+    }
     
     
 }
