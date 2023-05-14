152ae99ee26bb66047aad37523b5119d80153b69
jinan <ja.choi@sweetk.co.kr>
2023-05-14 16:25:57 +0900

    1. storyboard에 ui 생성

---
+++ SpotifyLoginSampleApp/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  SpotifyLoginSampleApp
+//
+//  Created by 최진안 on 2023/05/14.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //navigationBar 숨기기
+        navigationController?.navigationBar.isHidden = true
+        
+    }
+    
+    
+    
+    
+    
+}
