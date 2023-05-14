77bd0f7d3acf5195bb7d6395582132658a2ac3bd
jinan <ja.choi@sweetk.co.kr>
2023-05-14 17:40:22 +0900

    1. mainViewController추가

---
+++ SpotifyLoginSampleApp/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  SpotifyLoginSampleApp
+//
+//  Created by 최진안 on 2023/05/14.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+
+
+}
