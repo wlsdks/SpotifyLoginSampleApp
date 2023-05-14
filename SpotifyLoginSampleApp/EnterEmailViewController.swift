07cf03ff7a5f0d663aaef47f76c86014b2904288
jinan <ja.choi@sweetk.co.kr>
2023-05-14 18:18:09 +0900

    1. 계정이 이미 존재하면 생기는 오류코드를 가지고 예외처리를 만들어서 이미 있으면 login함수를 실행시키고 없으면 계정을 새로 만들도록 코드를 수정함 2. 위의 과정에 에러코드도 보여주도록 수정함

--- SpotifyLoginSampleApp/EnterEmailViewController.swift
+++ SpotifyLoginSampleApp/EnterEmailViewController.swift
@@ -39,7 +39,7 @@ class EnterEmailViewController: UIViewController {
         navigationController?.navigationBar.isHidden = false
     }
     
-    
+    // MARK: - 로그인 버튼 누르면 동작
     @IBAction func nextButtonTapped(_ sender: UIButton) {
         // Firebase 이메일/비밀번호 인증 로직
         let email = emailTextField.text ?? ""
@@ -49,7 +49,19 @@ class EnterEmailViewController: UIViewController {
         Auth.auth().createUser(withEmail: email, password: password) { [weak self] authResult, error in
             guard let self = self else { return }
             
-            self.showMainViewController()
+            if let error = error {
+                let code = (error as NSError).code
+                switch code {
+                case 17007: // 이미 가입한 계정일때 생기는 오류코드
+                    // 이미 있는 계정이니 로그인시키기
+                    self.loginUser(withEmail: email, password: password)
+                default:
+                    // 에러 메시지 표시시키기
+                    self.errorMessageLabel.text = error.localizedDescription
+                }
+            } else { // 아무런 에러가 없으면 그냥 이동
+                self.showMainViewController()
+            }
         }
     }
     
@@ -60,6 +72,17 @@ class EnterEmailViewController: UIViewController {
         navigationController?.show(mainViewController, sender: nil)
     }
     
+    private func loginUser(withEmail email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] _, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                self.errorMessageLabel.text = error.localizedDescription
+            } else {
+                self.showMainViewController()
+            }
+        }
+    }
 
 }
 
